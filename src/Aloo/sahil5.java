72fca4f29fe8c4bf33032b120af4f8f1c8a919b8
SSTR2000 <sahil.tomar_cs18@gla.ac.in>
2019-09-01 20:12:06 +0530

    use of scanner and Scanner is done successfully.

---
+++ src/Aloo/sahil5.java
@@ -0,0 +1,12 @@
+package Aloo;
+
+import java.util.Scanner;
+
+public class sahil5 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        String username = scanner.nextLine().trim();
+        System.out.println("Welcome," + username + "!");
+        scanner.close();
+    }
+}
