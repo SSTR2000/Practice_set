1be0dd398ad30ada5efd8dcff63783b58bba5eee
SSTR2000 <sahil.tomar_cs18@gla.ac.in>
2019-09-01 20:17:10 +0530

    the two strings are added successfully.

---
+++ src/Aloo/sahil6.java
@@ -0,0 +1,12 @@
+package Aloo;
+
+import java.util.Scanner;
+
+public class sahil6 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        String a = scanner.nextLine().trim();
+        String b = scanner.nextLine().trim();
+        System.out.println(a + b);
+    }
+}
