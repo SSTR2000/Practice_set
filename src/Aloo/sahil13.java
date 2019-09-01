84e3f004ef9398b90acefb61d96691059a9db1c8
SSTR2000 <sahil.tomar_cs18@gla.ac.in>
2019-09-02 00:32:34 +0530

    another if statement added

--- src/Aloo/sahil13.java
+++ src/Aloo/sahil13.java
@@ -7,13 +7,16 @@ public class sahil13 {
         Scanner scanner = new Scanner(System.in);
         System.out.println("Plese enter the no. :-");
         int a = scanner.nextInt();
-        int i;
+        int i, b = 0;
         for (i = 2; i < a; i++) {
             if (a % i == 0) {
                 System.out.println(a + " is not prime");
+                b = 1;
                 break;
             }
         }
+        if (b == 0) {
             System.out.println(a + " is prime");
+        }
     }
 }
