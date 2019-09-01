804fe3f211f37e1725248b513f00f4f69c14518b
SSTR2000 <sahil.tomar_cs18@gla.ac.in>
2019-09-01 23:24:53 +0530

    month by number

---
+++ src/Aloo/sahil11.java
@@ -0,0 +1,39 @@
+package Aloo;
+
+import java.util.Scanner;
+
+public class sahil11 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Plese enter the no.:-");
+        int a = scanner.nextInt();
+        if (a == 1) {
+            System.out.println("JANUARY");
+        } else if (a == 2) {
+            System.out.println("FEBRUARY");
+        } else if (a == 3) {
+            System.out.println("MARCH");
+        } else if (a == 4) {
+            System.out.println("APRIL");
+        } else if (a == 5) {
+            System.out.println("MAY");
+        } else if (a == 6) {
+            System.out.println("JUNE");
+        } else if (a == 7) {
+            System.out.println("JULY");
+        } else if (a == 8) {
+            System.out.println("AUGUST");
+        } else if (a == 9) {
+            System.out.println("SEPTEMBER");
+        } else if (a == 10) {
+            System.out.println("OCTOBER");
+        } else if (a == 11) {
+            System.out.println("NOVEMBER");
+        } else if (a == 12) {
+            System.out.println("DECEMBEER");
+        } else {
+            System.out.println("INVALID NUMBER");
+        }
+
+    }
+}
