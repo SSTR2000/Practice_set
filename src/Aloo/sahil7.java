9343fb687bfa594d81cdc9553727f71e03f03f62
SSTR2000 <sahil.tomar_cs18@gla.ac.in>
2019-09-01 20:25:49 +0530

    the arithematic operators are completed.

---
+++ src/Aloo/sahil7.java
@@ -0,0 +1,18 @@
+package Aloo;
+
+import java.util.Scanner;
+
+public class sahil7 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int a = scanner.nextInt();
+        int b = scanner.nextInt();
+        ;
+        System.out.println(a + "+" + b + "=" + a + b);
+        System.out.println(a + "*" + b + "=" + a * b);
+        System.out.println(a + "-" + b + "=" + (a - b);
+        System.out.println(a + "/" + b + "=" + a / b);
+        System.out.println(a + "%" + b + "=" + a % b);
+
+    }
+}
